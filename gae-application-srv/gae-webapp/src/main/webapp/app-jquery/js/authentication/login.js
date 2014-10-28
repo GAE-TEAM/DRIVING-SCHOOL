539c6814ed6d8fd7d4b10d388da0730adec2bf19
ML11181N <ML11181N@ZE0PW11T.2kze0.ze0.erd.edf.fr>
2014-10-28 18:26:19 +0100

    Ajout de la page login .

---
+++ gae-application-srv/gae-webapp/src/main/webapp/app-jquery/js/authentication/login.js
@@ -0,0 +1,37 @@
+//Login
+$(document).ready(function() {
+
+	$('#loginForm').bootstrapValidator({
+
+		feedbackIcons : {
+			valid : 'glyphicon glyphicon-ok',
+			invalid : 'glyphicon glyphicon-remove',
+			validating : 'glyphicon glyphicon-refresh'
+		},
+		fields : {
+			username : {
+
+				validators : {
+					notEmpty : {
+						message : 'The username is required'
+					}
+				}
+
+			},
+			password : {
+
+				validators : {
+					notEmpty : {
+						message : 'The password is required'
+					}
+				}
+
+			}
+		}
+	});
+	// var login = $('#login').val();
+	// var password = $('#password').val();
+
+	// console.log(login);
+
+});
